079aeaa414739eca23567c9cfbc57e6ce89a7593
abuhviktor <abuhviktor@gmail.com>
2023-04-27 05:22:22 +0100

    Simple Shell Group Commit

---
+++ errors.c
@@ -0,0 +1,88 @@
+#include "shell.h"
+
+/**
+ *_eputs - print an input string
+ * @str: the string to print
+ *
+ * Return: Nothing
+ */
+
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - write character c to stderr
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * an error, -1 is returned, and error is set
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - write character c to given fd
+ * @c: The character to print
+ * @fd: The filedescriptor to write
+ *
+ * Return: On success 1.
+ * an error, -1 is returned, and error is set
+ */
+
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ *_putsfd - print an input string
+ * @str: the string to print
+ * @fd: the filedescriptor to write
+ *
+ * Return: the number of chars
+ */
+
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
